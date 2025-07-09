2a3d7f3db91e69492e9ddbccc6b4a22f32a8e740
zedeska <ilovebigballs@eurocuck.pw>
2025-07-09 18:05:12 +0200

    oups

--- database/structs.go
+++ database/structs.go
@@ -1,15 +1,15 @@
 package db
 
 type Track struct {
-	ID         int
-	Title      string
-	Path       string
-	Filename   string
-	Artist     string
-	Album      string
-	Year       int
-	Duration   int
-	Cover      string
-	SampleRate float32
-	Bitrate    int
+	ID         int     `json:"id"`
+	Title      string  `json:"title"`
+	Path       string  `json:"path"`
+	Filename   string  `json:"filename"`
+	Artist     string  `json:"artist"`
+	Album      string  `json:"album"`
+	Year       int     `json:"year"`
+	Duration   int     `json:"duration"`
+	Cover      string  `json:"cover"`
+	SampleRate float32 `json:"sample_rate"`
+	Bitrate    int     `json:"bitrate"`
 }
