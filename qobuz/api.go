ef5cba1e097913af9629451271f786eeff442108
zedeska <ilovebigballs@eurocuck.pw>
2025-07-25 01:40:05 +0200

    jnjn

--- qobuz/api.go
+++ qobuz/api.go
@@ -180,6 +180,7 @@ func GetArtist(id string) (db.Artist, error) {
 	}
 
 	res := request.RunRest()
+	fmt.Println(res.Body)
 	if res.Error != nil || res.Response.StatusCode != 200 {
 		return db.Artist{}, errors.New("Error fetching artist")
 	}
