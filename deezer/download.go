85c9e1dc1c5e8c458bb940bae2b37d1c91bec649
zedeska <ilovebigballs@eurocuck.pw>
2025-10-03 14:36:22 +0200

    removed debug

--- deezer/download.go
+++ deezer/download.go
@@ -10,8 +10,6 @@ func Download(trackID int, path string) error {
 	cmd := exec.Command("python3", "orpheus.py", "-o", path, "download", "deezer", "track", fmt.Sprintf("%d", trackID))
 	homedir, _ := os.UserHomeDir()
 	cmd.Dir = homedir + "/OrpheusDL"
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	return err
 }
