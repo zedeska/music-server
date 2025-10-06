9727c2398971fed5fd225ae6ab55d9f9ff6d27cb
zedeska <ilovebigballs@eurocuck.pw>
2025-10-06 15:19:00 +0200

    fix

--- deezer/download.go
+++ deezer/download.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"sync"
 )
 
+var downloadMutex sync.Mutex
+
 func Download(trackID int, path string) error {
+	downloadMutex.Lock()
+	defer downloadMutex.Unlock()
+
 	cmd := exec.Command("python3", "orpheus.py", "-o", path, "download", "deezer", "track", fmt.Sprintf("%d", trackID))
 	homedir, _ := os.UserHomeDir()
 	cmd.Dir = homedir + "/OrpheusDL"
