a8eabff129996c432c064709b5cf8b69a5f97ba5
zedeska <ilovebigballs@eurocuck.pw>
2025-10-20 15:22:58 +0200

    qobuz back

--- main.go
+++ main.go
@@ -534,51 +534,48 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	/*
-		results_qobuz, err := qobuz.Search(query)
-		if err != nil {
-			http.Error(w, "Invalid search query", http.StatusBadRequest)
-			return
-		}
-	*/
+	results_qobuz, err := qobuz.Search(query)
+	if err != nil {
+		http.Error(w, "Invalid search query", http.StatusBadRequest)
+		return
+	}
+
 	results_deezer, err := deezer.Search(query)
 	if err != nil {
 		http.Error(w, "Invalid search query", http.StatusBadRequest)
 		return
 	}
 
-	/*
-		filteredQobuzTracks := make([]db.Track, 0, len(results_qobuz.Tracks))
-		for _, q := range results_qobuz.Tracks {
-			dup := false
-			for _, d := range results_deezer.Tracks {
-				if utils.Normalize(d.Album) == utils.Normalize(q.Album) && utils.Normalize(d.Artist) == utils.Normalize(q.Artist) && utils.Normalize(d.Title) == utils.Normalize(q.Title) {
-					dup = true
-					break
-				}
-			}
-			if !dup {
-				filteredQobuzTracks = append(filteredQobuzTracks, q)
+	filteredQobuzTracks := make([]db.Track, 0, len(results_qobuz.Tracks))
+	for _, q := range results_qobuz.Tracks {
+		dup := false
+		for _, d := range results_deezer.Tracks {
+			if utils.Normalize(d.Album) == utils.Normalize(q.Album) && utils.Normalize(d.Artist) == utils.Normalize(q.Artist) && utils.Normalize(d.Title) == utils.Normalize(q.Title) {
+				dup = true
+				break
 			}
 		}
+		if !dup {
+			filteredQobuzTracks = append(filteredQobuzTracks, q)
+		}
+	}
 
-		filteredQobuzAlbums := make([]db.Album, 0, len(results_qobuz.Albums))
-		for _, q := range results_qobuz.Albums {
-			dup := false
-			for _, d := range results_deezer.Albums {
-				if utils.Normalize(d.Title) == utils.Normalize(q.Title) && utils.Normalize(d.Artist) == utils.Normalize(q.Artist) {
-					dup = true
-					break
-				}
-			}
-			if !dup {
-				filteredQobuzAlbums = append(filteredQobuzAlbums, q)
+	filteredQobuzAlbums := make([]db.Album, 0, len(results_qobuz.Albums))
+	for _, q := range results_qobuz.Albums {
+		dup := false
+		for _, d := range results_deezer.Albums {
+			if utils.Normalize(d.Title) == utils.Normalize(q.Title) && utils.Normalize(d.Artist) == utils.Normalize(q.Artist) {
+				dup = true
+				break
 			}
 		}
+		if !dup {
+			filteredQobuzAlbums = append(filteredQobuzAlbums, q)
+		}
+	}
 
-		results_deezer.Tracks = append(results_deezer.Tracks, filteredQobuzTracks...)
-		results_deezer.Albums = append(results_deezer.Albums, filteredQobuzAlbums...)
-	*/
+	results_deezer.Tracks = append(results_deezer.Tracks, filteredQobuzTracks...)
+	results_deezer.Albums = append(results_deezer.Albums, filteredQobuzAlbums...)
 
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
@@ -664,7 +661,7 @@ func downloadTrack(id int, platform string) (string, string, error) {
 	var err error
 
 	if platform == "qobuz" {
-		err = qobuz.Download(id, MAX_QUALITY, temp_path) // Download to temp file
+		err = qobuz.Download(id, temp_path) // Download to temp file
 		if err != nil {
 			os.Remove(temp_path) // Clean up on failure
 			return "", "", errors.New("failed to cache track")
@@ -681,7 +678,7 @@ func downloadTrack(id int, platform string) (string, string, error) {
 			return "", "", fmt.Errorf("failed to get track IDs: %w", err)
 		}
 		if ids[0] != 0 {
-			err = qobuz.Download(ids[0], MAX_QUALITY, temp_path)
+			err = qobuz.Download(ids[0], temp_path)
 		} else if ids[1] != 0 {
 			err = deezer.Download(ids[1], temp_path)
 		} else {
--- qobuz/download.go
+++ qobuz/download.go
@@ -2,99 +2,15 @@ package qobuz
 
 import (
 	"fmt"
-	"music-server/utils"
-	"strconv"
-
-	"github.com/opensaucerer/goaxios"
+	"os/exec"
+	"path/filepath"
 )
 
-var DAB_API_URL string = "https://dab.yeet.su/api/"
-var SQUID_API_URL string = "https://qobuz.squid.wtf/api/"
+func Download(trackID int, path string) error {
+	filename := filepath.Base(path)
+	dir := filepath.Dir(path)
 
-type DabDLResult struct {
-	Url string `json:"url"`
-}
-
-type SquidDLResult struct {
-	Success bool `json:"success"`
-	Data    struct {
-		Url string `json:"url"`
-	} `json:"data"`
-}
-
-func dabDownload(id int, quality string, path string) error {
-	request := goaxios.GoAxios{
-		Url: DAB_API_URL + "stream",
-		Query: map[string]string{
-			"quality": quality,
-			"trackId": strconv.Itoa(id),
-		},
-		Headers: map[string]string{
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:139.0) Gecko/20100101 Firefox/139.0",
-		},
-		Method:         "GET",
-		ResponseStruct: &DabDLResult{},
-	}
-
-	res := request.RunRest()
-
-	if res.Error != nil || res.Response.StatusCode != 200 {
-		return fmt.Errorf("error fetching download URL: %w", res.Error)
-	}
-
-	result, _ := res.Body.(*DabDLResult)
-
-	err := utils.DownloadAndCheckTime(path, result.Url)
-	if err != nil {
-		return fmt.Errorf("error downloading and checking file: %w", err)
-	}
-
-	return nil
-}
-
-func squidDownload(id int, quality string, path string, api string) error {
-	request := goaxios.GoAxios{
-		Url: api + "download-music",
-		Query: map[string]string{
-			"quality":  quality,
-			"track_id": strconv.Itoa(id),
-		},
-		Method: "GET",
-		Headers: map[string]string{
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:139.0) Gecko/20100101 Firefox/139.0",
-		},
-		ResponseStruct: &SquidDLResult{},
-	}
-
-	res := request.RunRest()
-
-	if res.Error != nil || res.Response.StatusCode != 200 {
-		return fmt.Errorf("error fetching download URL: %w", res.Error)
-	}
-
-	result, _ := res.Body.(*SquidDLResult)
-
-	if !result.Success {
-		return fmt.Errorf("failed to fetch download URL: %s", string(res.Bytes))
-	}
-
-	err := utils.DownloadAndCheckTime(path, result.Data.Url)
-	if err != nil {
-		return fmt.Errorf("error downloading and checking file: %w", err)
-	}
-
-	return nil
-}
-
-func Download(id int, quality string, path string) error {
-
-	err := squidDownload(id, quality, path, SQUID_API_URL)
-	if err != nil {
-		err = dabDownload(id, quality, path)
-		if err != nil {
-			return fmt.Errorf("error downloading track: %w", err)
-		}
-	}
-
-	return nil
+	cmd := exec.Command("rip", "-f", dir, "-o", filename, "id", "qobuz", "track", fmt.Sprintf("%d", trackID))
+	err := cmd.Run()
+	return err
 }
